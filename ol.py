595c7be3b3891808434b5cdbd50e0fccb42b2cc1
igoryha <96ilik@mail.ru>
2023-08-26 10:47:53 +0300

    add bot code

--- ol.py
+++ ol.py
@@ -1,11 +1,28 @@
 import requests
+import time
 
 
-api_url = 'http://api.open-notify.org/iss-now.json'
+API_URL: str = 'https://api.telegram.org/bot'
+BOT_TOKEN: str = '5385998740:AAGTl4Jv7YeLmZhPcMuhX02vyHHpt7jyqIE'
+TEXT: str = 'как тебе такое \U0001FAE5'
+MAX_COUNTER: int = 10
 
-response = requests.get(api_url)   # Отправляем GET-запрос и сохраняем ответ в переменной response
+offset: int = -2
+counter: int = 0
+chat_id: int
 
-if response.status_code == 200:    # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
-    print(response.text)
-else:
-    print(response.status_code)    # При другом коде ответа выводим этот код
+
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
